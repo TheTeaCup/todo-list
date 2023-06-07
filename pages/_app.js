c605921b0549c676b9650e4dc5267d2912a88dac
TheTeaCup <lw0482352@gmail.com>
2023-06-06 22:51:49 -0400

    Added Chakra-UI to the page...

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
+import { ChakraProvider } from '@chakra-ui/react'
+import Head from 'next/head';
 import '@/styles/globals.css'
-
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+      <ChakraProvider>
+        <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1"/>
+          <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico"/>
+        </Head>
+        <Component {...pageProps} />
+      </ChakraProvider>
+  )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,27 @@
-
-export default function Home() {
+import {getCookie} from "cookies-next";
+export default function Home({userToken}) {
   return (
     <>
 
     </>
   )
 }
+
+export const getServerSideProps = ({req, res}) => {
+  let user = getCookie('token', {req, res}) || null;
+
+  if (user) {
+    return {
+      redirect: {
+        destination: '/@app',
+        permanent: false,
+      },
+    }
+  }
+
+  return {
+    props: {
+      userToken: getCookie('token', {req, res}) || null,
+    }
+  };
+};

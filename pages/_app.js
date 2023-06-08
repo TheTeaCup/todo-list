d26eeabf741c3124575934e6681e985f808a0ce9
TheTeaCup <lw0482352@gmail.com>
2023-06-08 00:29:45 -0400

    Theming updates

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,12 @@
-import { ChakraProvider } from '@chakra-ui/react'
+import {ChakraProvider, CSSReset} from '@chakra-ui/react'
 import Head from 'next/head';
 import '@/styles/globals.css'
+import theme from '@/utils/theme'
+
 export default function App({ Component, pageProps }) {
   return (
-      <ChakraProvider>
+      <ChakraProvider theme={theme}>
+          <CSSReset />
         <Head>
           <meta name="viewport" content="width=device-width, initial-scale=1"/>
           <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico"/>
---
+++ utils/theme.js
@@ -0,0 +1,18 @@
+import {extendTheme} from '@chakra-ui/react'
+import {mode} from '@chakra-ui/theme-tools';
+
+const theme = extendTheme({
+    config: {
+        initialColorMode: 'white',
+        useSystemColorMode: true,
+    },
+    styles: {
+        global: (props) => ({
+            body: {
+                bg: mode('gray.50', 'gray.900')(props),
+            }
+        })
+    }
+})
+
+export default theme

7f9e76fff486e4b89107f0d5e35727f603debcff
TheTeaCup <lw0482352@gmail.com>
2023-06-08 22:24:13 -0400

    Add: API Routes

---
+++ utils/api.js
@@ -0,0 +1,45 @@
+async function fetcher(url, options) {
+    const response = await fetch(`/api` + url, options);
+    return await response.json();
+}
+
+/*
+* Lets you login
+* */
+const login = async function (body) {
+    return await fetcher(`/auth/login`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(body),
+    });
+};
+
+/*
+* Creates a new account
+* */
+const register = async function (body) {
+    return await fetcher(`/auth/register`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(body),
+    });
+};
+
+/*
+* Logs you out
+* */
+const logout = async function (Token) {
+    return await fetcher(`/auth/logout`, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": Token || null,
+        },
+    });
+}
+
+export {fetcher, login, register, logout};

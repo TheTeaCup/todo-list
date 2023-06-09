5181e9b74304dd838129fe72fdc52e8736a3eb62
TheTeaCup <lw0482352@gmail.com>
2023-06-09 14:10:15 -0400

    Session update

--- utils/sessionSettings.js
+++ utils/sessionSettings.js
@@ -1,7 +1,7 @@
 // this file is a wrapper with defaults to be used in both API routes and `getServerSideProps` functions
 export const sessionOptions = {
     password: process.env.SESSION_SECRET,
-    cookieName: "todo-app",
+    cookieName: "todoApp",
     cookieOptions: {
         secure: process.env.NODE_ENV === 'production'
     },

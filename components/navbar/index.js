fbc5444b9e09f88f7bda2f510bc5b497967dc657
TheTeaCup <lw0482352@gmail.com>
2023-06-09 23:39:29 -0400

    Working on navbar

---
+++ components/navbar/index.js
@@ -0,0 +1,111 @@
+import React from 'react';
+import {
+    Box,
+    CloseButton,
+    Drawer,
+    DrawerContent,
+    Flex,
+    Icon,
+    Link,
+    Text,
+    useColorModeValue,
+    useDisclosure,
+} from '@chakra-ui/react';
+import {FiCompass, FiHome, FiSettings, FiStar, FiTrendingUp,} from 'react-icons/fi';
+import MobileNav from "@/components/navbar/mobile";
+
+const LinkItems = [
+    {name: 'Home', icon: FiHome},
+    {name: 'Create', icon: FiCompass},
+    {name: 'Favourites', icon: FiStar},
+    {name: 'Settings', icon: FiSettings},
+];
+
+export default function Navbar({
+                                              children,
+                                          }) {
+    const {isOpen, onOpen, onClose} = useDisclosure();
+    return (
+        <Box minH="100vh" bg={useColorModeValue('gray.100', 'gray.900')}>
+            <SidebarContent
+                onClose={() => onClose}
+                display={{base: 'none', md: 'block'}}
+            />
+            <Drawer
+                autoFocus={false}
+                isOpen={isOpen}
+                placement="left"
+                onClose={onClose}
+                returnFocusOnClose={false}
+                onOverlayClick={onClose}
+                size="full">
+                <DrawerContent>
+                    <SidebarContent onClose={onClose}/>
+                </DrawerContent>
+            </Drawer>
+            {/* mobilenav */}
+            <MobileNav onOpen={onOpen}/>
+            <Box ml={{base: 0, md: 60}} p="4">
+                {children}
+            </Box>
+        </Box>
+    );
+}
+
+const SidebarContent = ({onClose, ...rest}) => {
+    return (
+        <Box
+            transition="3s ease"
+            bg={useColorModeValue('white', 'gray.900')}
+            borderRight="1px"
+            borderRightColor={useColorModeValue('gray.200', 'gray.700')}
+            w={{base: 'full', md: 60}}
+            pos="fixed"
+            h="full"
+            {...rest}>
+            <Flex h="20" alignItems="center" mx="8" justifyContent="space-between">
+                <Text fontSize="2xl" fontFamily="monospace" fontWeight="bold">
+                    ToDo
+                </Text>
+                <CloseButton display={{base: 'flex', md: 'none'}} onClick={onClose}/>
+            </Flex>
+            {LinkItems.map((link) => (
+                <NavItem key={link.name} icon={link.icon}>
+                    {link.name}
+                </NavItem>
+            ))}
+        </Box>
+    );
+};
+
+const NavItem = ({icon, children, ...rest}) => {
+    return (
+        <Link href="#" style={{textDecoration: 'none'}} _focus={{boxShadow: 'none'}}>
+            <Flex
+                align="center"
+                p="4"
+                mx="4"
+                borderRadius="lg"
+                role="group"
+                cursor="pointer"
+                _hover={{
+                    bg: 'cyan.400',
+                    color: 'white',
+                }}
+                {...rest}>
+                {icon && (
+                    <Icon
+                        mr="4"
+                        fontSize="16"
+                        _groupHover={{
+                            color: 'white',
+                        }}
+                        as={icon}
+                    />
+                )}
+                {children}
+            </Flex>
+        </Link>
+    );
+};
+

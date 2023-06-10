4780a3c6d5354d34b32dbe8d5c59be1c62d5055d
TheTeaCup <lw0482352@gmail.com>
2023-06-09 23:56:56 -0400

    Whoops

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    env: {
-        SESSION_SECRET: process.env.SESSION_SECRET,
-        REDIS_URL: process.env.REDIS_URL,
-        CRYPTO_SECRET: process.env.CRYPTO_SECRET,
-        SITESALT: process.env.SITESALT,
-        SITEIV: process.env.SITEIV,
-    }
 }
 
 module.exports = nextConfig

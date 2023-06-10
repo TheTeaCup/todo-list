fae0b2050b0d3adc24502cdff8f6954d2879c363
TheTeaCup <lw0482352@gmail.com>
2023-06-09 23:39:47 -0400

    Add navbar to app

--- pages/app/index.js
+++ pages/app/index.js
@@ -2,13 +2,17 @@ import Head from "next/head";
 import csrf from "@/utils/csrf";
 import {sessionOptions} from "@/utils/sessionSettings";
 import {withIronSessionSsr} from "iron-session/next";
+import Navbar from "@/components/navbar";
 
-export default function Home({userToken, dataProps}) {
+export default function Home({user, dataProps}) {
+   console.log(user)
     return (
         <>
             <Head>
                 <title>ToDO App</title>
             </Head>
+
+            <Navbar/>
 
         </>
     )

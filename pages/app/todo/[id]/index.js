e7a760c24c4061876125600a470d7ce6968c69a6
TheTeaCup <lw0482352@gmail.com>
2023-06-11 21:09:50 -0400

    View and Edit pages for todo lists

---
+++ pages/app/todo/[id]/index.js
@@ -0,0 +1,37 @@
+import Head from "next/head";
+import csrf from "@/utils/csrf";
+import {sessionOptions} from "@/utils/sessionSettings";
+import {withIronSessionSsr} from "iron-session/next";
+import Navbar from "@/components/navbar";
+
+export default function Home({user, dataProps}) {
+    return (
+        <>
+            <Head>
+                <title>ToDO App</title>
+            </Head>
+
+            <Navbar user={user}/>
+
+        </>
+    )
+}
+
+export const getServerSideProps = withIronSessionSsr(async function ({req, res}) {
+    const user = req.session.user;
+
+    await csrf(req, res);
+
+    if (user === undefined) {
+        return {
+            redirect: {
+                destination: '/',
+                permanent: false,
+            },
+        }
+    } else {
+        return {
+            props: {user: req.session.user, csrfToken: req.csrfToken()}
+        };
+    }
+}, sessionOptions)

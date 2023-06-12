70f29ed4176e68b02452c6707a33691f90fa9f12
TheTeaCup <lw0482352@gmail.com>
2023-06-11 23:59:05 -0400

    Settings page

---
+++ pages/app/settings.js
@@ -0,0 +1,40 @@
+import Head from "next/head";
+import csrf from "@/utils/csrf";
+import {sessionOptions} from "@/utils/sessionSettings";
+import {withIronSessionSsr} from "iron-session/next";
+import Navbar from "@/components/navbar";
+
+export default function Home({user, dataProps}) {
+    console.log(user)
+    return (
+        <>
+            <Head>
+                <title>ToDo App</title>
+            </Head>
+
+            <Navbar user={user}>
+
+            </Navbar>
+
+        </>
+    )
+}
+
+export const getServerSideProps = withIronSessionSsr(async function ({req, res}) {
+    const user = req.session.user;
+
+    await csrf(req, res);
+
+    if (user === undefined) {
+        return {
+            redirect: {
+                destination: '/',
+                permanent: false,
+            },
+        }
+    } else {
+        return {
+            props: {user: req.session.user, csrfToken: req.csrfToken()}
+        };
+    }
+}, sessionOptions)
